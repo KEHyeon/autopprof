3640aac63a0e7a9b399eddb03cf6ec1cafcfa9c4
kth050213@naver.com <kth050213@naver.com>
2024-12-17 16:37:55 +0900

    feat: discordRepoter

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/daangn/autopprof
 
-go 1.19
+go 1.21
+
+toolchain go1.23.4
 
 require (
 	github.com/containerd/cgroups v1.0.4
@@ -11,11 +13,16 @@ require (
 require (
 	github.com/cilium/ebpf v0.4.0 // indirect
 	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
+	github.com/disgoorg/disgo v0.18.14 // indirect
+	github.com/disgoorg/json v1.2.0 // indirect
+	github.com/disgoorg/snowflake/v2 v2.0.3 // indirect
 	github.com/docker/go-units v0.4.0 // indirect
 	github.com/godbus/dbus/v5 v5.0.4 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/opencontainers/runtime-spec v1.0.2 // indirect
+	github.com/sasha-s/go-csync v0.0.0-20240107134140-fcbab37b09ad // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
-	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
 )
---
+++ report/discord.go
@@ -0,0 +1,67 @@
+package report
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"github.com/disgoorg/disgo/discord"
+	"github.com/disgoorg/disgo/webhook"
+)
+
+const (
+// reportTimeLayout = "2006-01-02T150405.MST"
+// cpuCommentFmt       = ":rotating_light:[CPU] usage (*%.2f%%*) > threshold (*%.2f%%*)"
+// memCommentFmt       = ":rotating_light:[MEM] usage (*%.2f%%*) > threshold (*%.2f%%*)"
+// goroutineCommentFmt = ":rotating_light:[GOROUTINE] count (*%d*) > threshold (*%d*)"
+)
+
+type DiscordReporter struct {
+	url     string
+	appName string
+	Client  webhook.Client
+}
+
+type DiscordReporterOption struct {
+	url     string
+	appName string
+}
+
+func DiscordSlackReporter(opt *DiscordReporterOption) (*DiscordReporter, error) {
+	client, err := webhook.NewWithURL(opt.url)
+	if err != nil {
+		return nil, err
+	}
+	return &DiscordReporter{
+		url:     opt.url,
+		appName: opt.appName,
+		Client:  client,
+	}, nil
+}
+
+// ReportCPUProfile sends the CPU profiling data to the Discord
+func (s *DiscordReporter) ReportCPUProfile(
+	ctx context.Context, r io.Reader, ci CPUInfo,
+) error {
+	message := discord.NewWebhookMessageCreateBuilder().SetContent("test").SetUsername("문진진").Build()
+	_, err := s.Client.CreateMessage(message)
+	if err != nil {
+		fmt.Println("메시지 전송 실패:", err)
+		return err
+	}
+	return nil
+}
+
+// ReportHeapProfile sends the heap profiling data to the Discord.
+func (s *DiscordReporter) ReportHeapProfile(
+	ctx context.Context, r io.Reader, mi MemInfo,
+) error {
+	return nil
+}
+
+// ReportGoroutineProfile sends the goroutine profiling data to the Discord.
+func (s *DiscordReporter) ReportGoroutineProfile(
+	ctx context.Context, r io.Reader, gi GoroutineInfo,
+) error {
+	return nil
+}
